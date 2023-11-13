ecb97f9298f11d34ba9d434bb6519ec4e3313798
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-11-13 21:08:47 +0700

    update entity

--- domain/src/main/java/org/example/domain/entities/Student.java
+++ domain/src/main/java/org/example/domain/entities/Student.java
@@ -9,33 +9,16 @@ import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.util.Date;
 import java.util.UUID;
 
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "student")
 @Where(clause = "is_deleted = false")
 @AttributeOverride(name = "id", column = @Column(name = "student_id"))
 public class Student extends BaseEntity {
-    public Student(UUID id, String name, String email, String password, String phone, String address) {
-        super(id);
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.phone = phone;
-        this.address = address;
-    }
-
-    public Student(String name, String email, String password, String phone, String address) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.phone = phone;
-        this.address = address;
-    }
-
     @Column(name = "student_name")
     private String name;
 
@@ -50,4 +33,13 @@ public class Student extends BaseEntity {
 
     @Column(name = "student_address")
     private String address;
+
+    public Student(UUID id, String name, String email, String password, String phone, String address) {
+        super(id);
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+    }
 }
